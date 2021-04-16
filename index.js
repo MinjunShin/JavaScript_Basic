e3b80d9bb3e4b81b853685e0e4bacd0f58f3fce1
MinjunShin <pj0014@naver.com>
2021-04-16 23:24:54 +0900

    -m "String"

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const a = 221;
+const a = 221; // 첫사용은 const로, let은 정말 필요할 때만.
 let b = a - 5;
 a = 4;
 console.log(b, a);
 console.log(a, b);
+
+const what = "HY";
+console.log(what);
+
+// to do : finish this (single line comment)
+/* multi
+ line 
+ comment
+ */ 
