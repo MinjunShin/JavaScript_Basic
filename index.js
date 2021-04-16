1aa876da9ee3381f0df0d1bf04270c8e82d20d8c
MinjunShin <pj0014@naver.com>
2021-04-16 23:28:15 +0900

    const의 종류

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ console.log(a, b);
 const what = "HY";
 console.log(what);
 
+const wat = true; // true, false는 text가 아닌 boolean이다.
+
+const floatEx = 55.1;
+
+
+
 // to do : finish this (single line comment)
 /* multi
  line 
