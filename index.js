e83262f8c9a48cfbae133455437272ae480b4361
MinjunShin <mj.irin1260@gmail.com>
2021-05-17 23:19:06 +0900

    Modifying the DOM with JS

--- index.js
+++ index.js
@@ -1,63 +1,9 @@
 
-const title = document.getElementById("title");
+const title = document.querySelector(".title");
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
 
 console.log(title);
+console.dir(document);
 
-title.innerHTML = "Hi! From JS";
-
-
-/*
-const a = 221; // 첫사용은 const로, let은 정말 필요할 때만.
-let b = a - 5;
-
-console.log(b, a);
-console.log(a, b);
-
-const what = "HY";
-console.log(what);
-
-const wat = true; // true, false는 text가 아닌 boolean이다.
-
-const floatEx = 55.1;
-
-const PersonInfo = {
-	name : "SMJ",
-	age : 28,
-	gender : "Male",
-	isHandsome : true,
-	favMovies : ["Along the gods", "End game"],
-	favFood : [
-		{
-				name : "Kimchi", 
-				fatty : false
-		}, 
-		{
-				name : "Cheeseburger", 
-				fatty : true
-		}
-	]
-}
-
-console.log(PersonInfo);
-console.log(PersonInfo.age);
-
-console.log(console);
-
-function sayHello(name, age) {
-	console.log("Hello" + name + "you are" + age);
-	console.log(`Hello ${name} you are ${age} years old`);
-}
-
-const greetMinjun = sayHello("Minjun", 28)
-
-console.log(greetMinjun)
-
-const calculator = {
-	plus_cal : function(a, b) {
-		return a + b;
-	}
-}
-
-const plus = calculator.plus_cal(5, 5)
-console.log(plus)
-*/
+document.title = "I own you now";
