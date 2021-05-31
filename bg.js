77e3278c2552b0bd2a3a174b5b8290b7495ea1f1
MinjunShin <mj.irin1260@gmail.com>
2021-05-31 23:21:10 +0900

    Image Background complete

--- bg.js
+++ bg.js
@@ -1,21 +1,22 @@
 const body = document.querySelector("body");
-const IMG_NUMBER = 5;
+
+const IMG_NUMBER = 3;
 
 function paintImage(imgNumber) {
-    const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`; // 랜덤함수가 0을 줄 수 있으므로 +1
-    image.classList.add("bgimage");
-    body.prepend(image);
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`; // 랜덤함수가 0을 줄 수 있으므로 +1
+  image.classList.add("bgimage");
+  body.prepend(image);
 }
 
 function genRandom() {
-    const number = Math.floor(Math.random() * IMG_NUMBER);
-    return number;
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
 }
 
 function init() {
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
 }
 
 init();
