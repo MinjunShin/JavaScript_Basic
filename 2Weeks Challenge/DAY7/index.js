38f08e82d53004990e5b6c7102c1086d38db6662
MinjunShin <pj0014@naver.com>
2021-06-08 22:05:13 +0900

    ongoing

--- 2Weeks Challenge/DAY7/index.js
+++ 2Weeks Challenge/DAY7/index.js
@@ -3,35 +3,66 @@ const todoForm = document.querySelector(".todo-form"),
   todoList = document.querySelector(".todo-List");
 
 const TODO_LS = "toDos";
+let toDos = [];
 
 function paintToDo(text) {
+  const newId = toDos.length + 1;
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerText = "❌";
-  const span = document.createElement("span");
-  span.innerText = "text";
+  delBtn.addEventListener("click", deleteToDo);
+  const span = document.createElement("span"); 
+  span.innerText = `${text}`;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
   todoList.appendChild(li);
-  
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function filterFn(toDo) {
+  return toDo.id === 1;
+}
+
+function deleteToDo(event) {
+  const clicked = event.target;
+  const selectedli = clicked.parentNode;
+  todoList.removeChild(selectedli);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(selectedli.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() {
+  localStorage.setItem(TODO_LS, JSON.stringify(toDos))
 }
 
 function handleSubmit(event) {
-  event.PreventDefault();
+  event.preventDefault();
   const currentValue = todoInput.value;
-  paintToDo(currentValue);    
+  paintToDo(currentValue);   
 }
 
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODO_LS);
   if (loadedToDos !== null) {
-
+    const parsedToDo = JSON.parse(loadedToDos);
+    parsedToDo.forEach(function(toDo){
+      paintToDo(toDo.text);
+    });
   }
 }
 
 function init() {
   loadToDos();
-  todoForm.addEventListener("Submit", handleSubmit());
+  todoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
