a5c41930287605e6f625148632ba00cc9003ad18
MinjunShin <mj.irin1260@gmail.com>
2021-06-29 04:12:02 +0900

    test commit2

--- Programmers LV1/lesson_12997.js
+++ Programmers LV1/lesson_12997.js
@@ -1,19 +1,21 @@
+let numArr = [1, 3, 4, 7];
+
 function solution(nums) {
   var answer = -1;
   // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
   let sum = 0;
   let result = [];
-
-  if(result.length === 0){
+  if (result.length === 0) {
     answer = -1;
-  } else {   
-
-    for(i = 0; i<nums.length; i++) {
-      nums.array.forEach(element => {
-      sum += element;     
-      });      
+  } else {
+    for (i = 0; i < nums.length; i++) {
+      nums.array.forEach((element) => {
+        sum += element;
+      });
     }
   }
-  
+
   return answer;
 }
+
+console.log(solution(numArr));
