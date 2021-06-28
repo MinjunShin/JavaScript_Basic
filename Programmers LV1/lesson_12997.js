3f0a30fa80c5a4d9ae0c105a48b30038d6e937db
MinjunShin <mj.irin1260@gmail.com>
2021-06-28 20:38:14 +0900

    소수구하기 문제풀이

---
+++ Programmers LV1/lesson_12997.js
@@ -0,0 +1,20 @@
+function solution(nums) {
+  var answer = -1;
+  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+  let sum = 0;
+  let result = [];
+
+  if(result.length === 0){
+    answer = -1;
+  } else {   
+
+    for(i = 0; i<nums.length; i++) {
+      nums.array.forEach(element => {
+      sum += element;     
+      });      
+    }
+
+  }
+  
+  return answer;
+}
